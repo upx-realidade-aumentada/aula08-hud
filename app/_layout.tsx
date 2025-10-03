0969cb6c8810393940a3373a2e0da8d0f835ab87
Thiago Santos <thiagodlsantos9@gmail.com>
2025-10-02 21:45:07 -0300

    refactor: change the stack to contain all pages within the folder

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,5 @@
 import { Stack } from "expo-router";
 
 export default function Layout() {
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="home" options={{}} />
-    </Stack>
-  );
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
